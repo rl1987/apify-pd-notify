d2f847320c994c9911e5d2a1d6deb85747d119da
rl1987 <rl1987@users.noreply.github.com>
2026-06-27 14:02:14 +0300

    Start new Apify actor

---
+++ pd-notify/my_actor/main.py
@@ -0,0 +1,23 @@
+"""Module defines the main entry point for the Apify Actor.
+
+Feel free to modify this file to suit your specific needs.
+
+To build Apify Actors, utilize the Apify SDK toolkit, read more at the official documentation:
+https://docs.apify.com/sdk/python
+"""
+
+from __future__ import annotations
+
+from apify import Actor
+
+
+async def main() -> None:
+    """Define a main entry point for the Apify Actor.
+
+    This coroutine is executed using `asyncio.run()`, so it must remain an asynchronous function for proper execution.
+    Asynchronous execution is required for communication with Apify platform, and it also enhances performance in
+    the field of web scraping significantly.
+    """
+    async with Actor:
+        Actor.log.info('Hello from the Actor!')
+        # Write your code here
